1e4c80645e2648559e3794b80ff14b0962b97de5
Evgen-Opanasenko <91693271+Evgen-Opanasenko@users.noreply.github.com>
2022-02-05 19:03:21 +0200

    на сдачу

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// {
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>; */
+// }
+const galleryRef = document.querySelector(".gallery");
 
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+galleryRef.innerHTML = galleryMarkup;
+
+galleryRef.addEventListener("click", onOpenModal);
+
+function onOpenModal(e) {
+  e.preventDefault();
+  if (e.target.tagName !== "IMG") {
+    // window.removeEventListener("keydown", escCloseModal);
+    return;
+  }
+
+  const originalImgSrc = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${originalImgSrc}" width="800" height="600">
+`);
+  instance.show();
+
+  window.addEventListener("keydown", escCloseModal);
+  function escCloseModal(e) {
+    console.log(e);
+    if (e.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", escCloseModal);
+    }
+  }
+
+  console.log(originalImgSrc);
+}
+// onClose: (instance) => {
+//   window.removeEventListener("keydown", escCloseModal);
+// };  не могу понять как применить
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+galleryRef.innerHTML = galleryMarkup;
 console.log(galleryItems);
+
+const lbGallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+// gallery.open();
+// gallery.on("show.simplelightbox", function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   if (e.target.tagName !== "IMG") {
+//     return;
+//   }
+
+//   //   const originalImgSrc = e.target.dataset.source;
+//   //   const instance = basicLightbox.create(`
+//   //     <img src="${originalImgSrc}" width="800" height="600">
+//   // `);
+//   //   instance.show();
+// });
+
+// gallery.on("error.simplelightbox", function (e) {
+//   console.log(e);tion
+// });
+
+// let gallery = $(".gallery a").simpleLightbox();
+// gallery.on("show.simplelightbox", function () {
+
+// });
